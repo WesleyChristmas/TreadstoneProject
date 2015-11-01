c09bbe16ff5545e7c07e03d3a07cc2382d722e5b
Илья <williamvinogradov@outlook.com>
2015-11-01 18:54:10 +0400

    Business interface fix

---
+++ Library/BusinessInterface/FoodMenuServiceBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BusinessEntity;
+using BusinessEntity.Models;
+
+namespace BusinessInterface
+{
+    public interface IFoodMenuServiceBase
+    {
+        List<FoodMenuTypeEntity> GetAllFoodMenuTypes();
+        void AddFoodMenuType(FoodMenuTypeEntity type, ReceiveFileModel image);
+        void UpdateMenuType(FoodMenuTypeEntity type, ReceiveFileModel image);
+        bool DeleteMenuType(int idType);
+    }
+}
