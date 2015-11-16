e1bb566faeaf23b82f9ccc04823b75fdd1f244db
Алексей <swtorigra@gmail.com>
2015-11-16 22:04:52 +0300

    Доделан календарь, все норм. Единственное, ему не нравятся пути к картинкам

--- PartyCafe.Site/Scripts/AngularControllers/EventCalendarContoller.js
+++ PartyCafe.Site/Scripts/AngularControllers/EventCalendarContoller.js
@@ -1,4 +1,18 @@
 ﻿var eventcalendarapp = new angular.module("eventcalendarapp", []);
+eventcalendarapp.directive('note', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            $timeout(function () {
+                var iso = new Isotope(document.querySelector('#calendar-wrap'), {
+                    itemSelector: '.event-day',
+                    layoutMode: 'masonry'
+                });
+            }, 100);
+        }
+    };
+});
+
 
 eventcalendarapp.controller("EventCalendar", function ($scope, $http) {
     $scope.Events = [];
@@ -13,22 +27,9 @@ function setCalendaData(_d, _m) {
 }
 
 function getData($scope, $http) {
-
     $http.get("/EventCalendar/GetCalendar").success(function (data, status) {
         Calendar('calendar-wrap', 30, data.Calendar, data.CurDate.replace(/\D+/g, ""), $scope);
     })
-
-    /*var xhr = new XMLHttpRequest();
-
-    xhr.open('GET', 'GetCalendar', true);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            var result = JSON.parse(xhr.response);
-            console.log(result);
-           // Calendar('calendar-wrap', 30, result.Calendar, result.CurDate.replace(/\D+/g, ""), $scope);
-        }
-    }*/
 }
 
 function Calendar(obj, dcount, respons, curdate, $scope) {
@@ -83,14 +84,101 @@ function Calendar(obj, dcount, respons, curdate, $scope) {
     }
     console.log(cal);
     $scope.Events = cal;
-    $scope.$apply();
+    //$scope.$apply();
 
-    var elem = document.querySelector('#calendar-wrap');
+    /*var elem = document.querySelector('#calendar-wrap');
     var iso = new Isotope(elem, {
         itemSelector: '.event-day',
         layoutMode: 'masonry'
     });
 
+    setTimeout(function () {
+        iso.layout(cal);
+    }, 100);*/
+
+}
+
+function Masonry() {
+    var $container = $('#calendar-wrap'),
+        isotopeOptions = {},
+        defaultOptions = {
+            sortBy: 'original-order',
+            sortAscending: true,
+            layoutMode: 'masonry'
+        };
+
+    $container.isotope({
+        itemSelector: '.event-day',
+    });
+
+    var $optionSets = $('#options').find('.option-set'),
+        isOptionLinkClicked = false;
+
+    // switches selected class on buttons
+    function changeSelectedLink($elem) {
+        // remove selected class on previous item
+        $elem.parents('.option-set').find('.selected').removeClass('selected');
+        // set selected class on new item
+        $elem.addClass('selected');
+    }
+
+    $optionSets.find('a[href^="#filter"]').click(function () {
+        var $this = $(this);
+        // don't proceed if already selected
+        if ($this.hasClass('selected')) { return; }
+
+        changeSelectedLink($this);
+        // get href attr, remove leading #
+        var href = $this.attr('href').replace(/^#/, ''),
+            // convert href into object
+            // i.e. 'filter=.inner-transition' -> { filter: '.inner-transition' }
+            option = $.deparam(href, true);
+
+        // apply new option to previous
+        $.extend(isotopeOptions, option);
+        // set hash, triggers hashchange on window
+        $.bbq.pushState(isotopeOptions);
+        isOptionLinkClicked = true;
+        return false;
+    });
+
+    var hashChanged = false;
+
+    $(window).bind('hashchange', function (event) {
+        // get options object from hash
+        var hashOptions = window.location.hash ? $.deparam.fragment(window.location.hash, true) : {},
+            // do not animate first call
+            aniEngine = hashChanged ? 'best-available' : 'none',
+            // apply defaults where no option was specified
+            options = $.extend({}, defaultOptions, hashOptions, { animationEngine: aniEngine });
+
+        // apply options from hash
+        $container.isotope(options);
+        // save options
+        isotopeOptions = hashOptions;
+
+        // if option link was not clicked
+        // then we'll need to update selected links
+        if (!isOptionLinkClicked) {
+            // iterate over options
+            var hrefObj, hrefValue, $selectedLink;
+            for (var key in options) {
+                hrefObj = {};
+                hrefObj[key] = options[key];
+                // convert object into parameter string
+                // i.e. { filter: '.inner-transition' } -> 'filter=.inner-transition'
+                hrefValue = $.param(hrefObj);
+                // get matching link
+                $selectedLink = $optionSets.find('a[href="#' + hrefValue + '"]');
+                changeSelectedLink($selectedLink);
+            }
+        }
+
+        isOptionLinkClicked = false;
+        hashChanged = true;
+    }).trigger('hashchange');
+
+    return false;
 }
 
 function fillCalendar(obj) {
