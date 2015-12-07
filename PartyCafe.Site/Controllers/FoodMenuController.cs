5aedff8d07d8533542d82614bf9be76febdb1496
Алексей <swtorigra@gmail.com>
2015-12-07 13:12:53 +0300

    Добавлены методы в контроллер меню

--- PartyCafe.Site/Controllers/FoodMenuController.cs
+++ PartyCafe.Site/Controllers/FoodMenuController.cs
@@ -1,13 +1,35 @@
 ﻿using System.Web.Mvc;
+using System.Collections.Generic;
+using BusinessEntity;
+using BusinessInterface;
 
 namespace PartyCafe.Site.Controllers
 {
     public class FoodMenuController : Controller
     {
+        private readonly IFoodMenuServiceBase _menuService;
+        public FoodMenuController(IFoodMenuServiceBase menuService)
+        {
+            _menuService = menuService;
+        }
+
         // GET: FoodMenu
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public JsonResult GetAllMenuTypes()
+        {
+            return Json(_menuService.GetAllFoodMenuTypes(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GetFoodMenu(int idType)
+        {
+            return Json(_menuService.GetFoodMenu(idType));
+        }
+
     }
 }
