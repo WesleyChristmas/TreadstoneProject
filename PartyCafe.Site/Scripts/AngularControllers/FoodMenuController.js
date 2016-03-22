5ffca53248f5036c00c0a2a04af7f13ee463d0b0
swtorigra@gmail.com <deletan25%test>
2016-03-23 00:49:04 +0300

    Заработал ангуляр, переделываю меню под ангуляр. Поменял стилии верстку

---
+++ PartyCafe.Site/Scripts/AngularControllers/FoodMenuController.js
@@ -0,0 +1,15 @@
+﻿var foodmenuapp = new angular.module("foodmenuapp", []);
+
+foodmenuapp.controller("FoodMenuCt", function ($scope, $http) {
+    $scope.Food = [];
+    $scope.title = 'Наше меню в кафе';
+    $scope.ShowSubMenu = function (obj) { ShowSubMenu(); }
+
+    GetAllMenu($scope, $http);
+});
+
+function GetAllMenu($scope, $http) {
+    $http.get("/FoodMenu/GetAllMenu").success(function (data, status) {
+        $scope.Food = data;
+    });
+}
