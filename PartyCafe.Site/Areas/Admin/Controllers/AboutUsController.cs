a99370657763a600bcc5b03ab60b00e0dee2319f
swtorigra@gmail.com <deletan25%test>
2016-05-02 13:36:09 +0300

    В админку добавлен контроллер для редактированяи страницы о нас. изменена верстка и стили страниц: услгуи, фотогалееря.

---
+++ PartyCafe.Site/Areas/Admin/Controllers/AboutUsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyCafe.Site.DBUtils;
+
+namespace PartyCafe.Site.Areas.Admin.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: Admin/AboutUs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AboutHome()
+        {
+            return View("AboutHome");
+        }
+        [HttpGet]
+        public ActionResult AboutAdd()
+        {
+            return View("AboutAdd");
+        }
+        [HttpGet]
+        public ActionResult AboutEdit()
+        {
+            return View("AboutEdit");
+        }
+
+
+        [HttpGet]
+        public JsonResult GetAllAbout()
+        {
+            var aboutus = ServiceUtils.GetAll(1);
+            return Json(aboutus, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [HttpPost]
+        public string UpdateAbout(int id, string name, string desc)
+        {
+            return null;
+           /* try
+            {
+                var request = Request.Files;
+                if (request.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    var content = new byte[file.ContentLength];
+                    var filename = file.FileName;
+                    file.InputStream.Read(content, 0, file.ContentLength);
+
+                    GalleryUtils.EditGallery(
+                        new PCService()
+                        {
+                            idRecord = id,
+                            name = name,
+                            description = desc
+                        },
+                        User.Identity.Name,
+                        new PCPhoto() { data = content, fileName = filename }
+                    );
+                    return "ok";
+                }
+                else
+                {
+                    ServiceUtils.EditService()
+                        new Party()
+                        {
+                            idRecord = id,
+                            name = name,
+                            description = desc
+                        },
+                        User.Identity.Name,
+                        null
+                    );
+                    return "ok";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Произошла ошибка! " + ex.Message.ToString();
+            }*/
+        }
+
+    }
+}
---
+++ PartyCafe.Site/Areas/Admin/Script/AboutController.js
@@ -0,0 +1,89 @@
+﻿var aboutapp = new angular.module("AboutApp", ['ngRoute']);
+
+aboutapp.config(function ($routeProvider) {
+    $routeProvider.when('/',
+    {
+        templateUrl: 'AboutUs/AboutHome',
+        controller: 'AboutHomeController'
+    }).when('/add', {
+        templateUrl: 'AboutUs/AboutAdd',
+        controller: 'AboutAddController'
+    }).when('/edit', {
+        templateUrl: 'AboutUs/AboutEdit',
+        controller: 'AboutEditController'
+    }).otherwise('/');
+});
+
+aboutapp.service("sharedDataService", function () {
+    this.rolesItem = {};
+
+    this.setItem = function (item) { this.rolesItem = item; }
+    this.getItem = function () { return this.rolesItem; }
+});
+
+/* About Home Controller */
+aboutapp.controller("AboutHomeController", function ($scope, $http, $location, sharedDataService) {
+    $scope.isActive = function (item) { return $scope.selectForEdit === item; };
+    $scope.HighlightItem = function (item) { $scope.selectForEdit = item; };
+    $scope.AddAbout = function () { $location.path('/add'); };
+    $scope.EditAbout = function () {
+        sharedDataService.setItem($scope.selectForEdit);
+        $location.path('/edit');
+    };
+    $scope.removeAboutItem = function (item) {
+        $http.post('AboutUs/DeleteAboutItem', { id: item.idRecord }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+
+    $scope.About = [];
+    $scope.Header = "Управление фотогалереей";
+    $scope.selectForEdit = '';
+
+    GetAllAbout($scope, $http);
+});
+
+/* About Edit Controller */
+aboutapp.controller("AboutEditController", function ($scope, $http, $location, $routeParams, sharedDataService) {
+    $scope.Header = "Редактирование блока";
+    $scope.itemForEdit = sharedDataService.getItem();
+
+    $scope.updateAbout = function () {
+        var fd = new FormData();
+        fd.append('id', $scope.itemForEdit.idRecord);
+        fd.append('name', $scope.itemForEdit.name);
+        fd.append('desc', $scope.itemForEdit.description);
+        fd.append('file', document.getElementsByName('galleryPhoto')[0].files[0]);
+
+        $http.post('About/UpdateAbout', fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+    $scope.BackToAboutList = function () {
+        $location.path('/');
+    };
+});
+
+
+function GetAllAbout($scope, $http) {
+    $('#loader').css({ "display": "block" });
+    $('.spinner').show();
+
+    $http.get('AboutUs/GetAllAbout').success(function (result) {
+        $scope.About = result;
+    });
+
+    $('#loader').css({ "display": "none" });
+    $('.spinner').hide();
+}
