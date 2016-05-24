b9fb6f865f6a19aad65b7bbd1edce196658a0681
Алексей <swtorigra@gmail.com>
2016-05-24 20:35:04 +0300

    Добавлен ServicesController.js, добавлена вьюха длядомашней страницы. Добавлены методы в контроллер

--- PartyCafe.Site/Areas/Admin/Controllers/ServicesController.cs
+++ PartyCafe.Site/Areas/Admin/Controllers/ServicesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyCafe.Site.DBUtils;
 
 namespace PartyCafe.Site.Areas.Admin.Controllers
 {
@@ -30,5 +31,175 @@ namespace PartyCafe.Site.Areas.Admin.Controllers
         }
 
 
+        [HttpPost]
+        public string AddServices(string name, string desc)
+        {
+            try
+            {
+                var request = Request;
+                if (request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+                    var content = new byte[file.ContentLength];
+                    var filename = file.FileName;
+                    file.InputStream.Read(content, 0, file.ContentLength);
+
+                    ServiceUtils.InsertService(
+                        new PCService()
+                        {
+                            name = name,
+                            description = desc,
+                            serviceType = 0
+                        },
+                        User.Identity.Name,
+                        new PCPhoto() { data = content, fileName = filename }
+                    );
+                    return "ok";
+                }
+                else
+                {
+                    ServiceUtils.InsertService(
+                        new PCService()
+                        {
+                            name = name,
+                            description = desc,
+                            serviceType = 0
+                        },
+                        User.Identity.Name,
+                        null
+                    );
+                    return "ok";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Произошла ошибка! " + ex.Message.ToString();
+            }
+        }
+        [HttpPost]
+        public string RemoveService(int id)
+        {
+            try
+            {
+                ServiceUtils.DelService(id);
+                return "ok";
+            }
+            catch (Exception ex)
+            {
+                return "Произошла ошибка! " + ex.Message.ToString();
+            }
+        }
+        [HttpPost]
+        public string UpdateAbout(int id, string name, string desc)
+        {
+            try
+            {
+                var request = Request.Files;
+                if (request.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    var content = new byte[file.ContentLength];
+                    var filename = file.FileName;
+                    file.InputStream.Read(content, 0, file.ContentLength);
+
+                    ServiceUtils.EditService(
+                        new PCService()
+                        {
+                            idRecord = id,
+                            name = name,
+                            description = desc
+                        },
+                        User.Identity.Name,
+                        new PCPhoto() { data = content, fileName = filename }
+                    );
+                    return "ok";
+                }
+                else
+                {
+                    ServiceUtils.EditService(
+                        new PCService()
+                        {
+                            idRecord = id,
+                            name = name,
+                            description = desc
+                        },
+                        User.Identity.Name,
+                        null
+                    );
+                    return "ok";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Произошла ошибка! " + ex.Message.ToString();
+            }
+        }
+
+
+        [HttpPost]
+        public string AddPhotoToBlock(int id, string name, string desc)
+        {
+            try
+            {
+                var request = Request;
+                if (request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+                    var content = new byte[file.ContentLength];
+                    var filename = file.FileName;
+                    file.InputStream.Read(content, 0, file.ContentLength);
+
+                    ServiceUtils.AddPhoto(
+                        id,
+                        name,
+                        new PCPhoto() { data = content, fileName = filename },
+                        User.Identity.Name
+                    );
+                    return "ok";
+                }
+                else
+                {
+                    return "bed";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Произошла ошибка! " + ex.Message.ToString();
+            }
+        }
+        [HttpPost]
+        public string RemovePhotoFromBlock(int id)
+        {
+            try
+            {
+                ServiceUtils.DelPhoto(id);
+                return "ok";
+            }
+            catch (Exception ex)
+            {
+                return "bed";
+            }
+        }
+
+        [HttpPost]
+        public JsonResult GetBlockPhotos(int id)
+        {
+            try
+            {
+                var aboutus = ServiceUtils.GetAll(ServiceType.originService).Where(w => w.idRecord == id).SingleOrDefault();
+                return Json(aboutus, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpGet]
+        public JsonResult GetAllServices()
+        {
+            var services = ServiceUtils.GetAll(ServiceType.originService);
+            return Json(services, JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ PartyCafe.Site/Areas/Admin/Script/ServicesController.js
@@ -0,0 +1,193 @@
+﻿var servicesapp = new angular.module("ServicesApp", ['ngRoute']);
+
+servicesapp.config(function ($routeProvider) {
+    $routeProvider.when('/',
+    {
+        templateUrl: 'Services/ServicesHome',
+        controller: 'ServicesHomeController'
+    }).when('/add', {
+        templateUrl: 'Services/ServicesAdd',
+        controller: 'ServicesAddController'
+    }).when('/edit', {
+        templateUrl: 'Services/ServicesEdit',
+        controller: 'ServicesEditController'
+    }).when('/editphoto', {
+        templateUrl: 'Services/ServicesEditPhoto',
+        controller: 'ServicesEditPhotoController'
+    }).otherwise('/');
+});
+
+servicesapp.service("sharedDataService", function () {
+    this.rolesItem = {};
+
+    this.setItem = function (item) { this.rolesItem = item; }
+    this.getItem = function () { return this.rolesItem; }
+});
+
+/* Services Home Controller */
+servicesapp.controller("ServicesHomeController", function ($scope, $http, $location, sharedDataService) {
+    $scope.isActive = function (item) { return $scope.selectForEdit === item; };
+    $scope.HighlightItem = function (item) { $scope.selectForEdit = item; };
+    $scope.addServices = function () { $location.path('/add'); };
+    $scope.editServices = function () {
+        sharedDataService.setItem($scope.selectForEdit);
+        $location.path('/edit');
+    };
+    $scope.editServicesPhoto = function () {
+        sharedDataService.setItem($scope.selectForEdit);
+        $location.path('/editphoto');
+    };
+    $scope.removeServices = function (item) {
+        $http.post('Services/RemoveServices', { id: item.idRecord }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+
+    $scope.Services = [];
+    $scope.Header = 'Управление разделом "Услуги"';
+    $scope.selectForEdit = '';
+
+    GetAllServices($scope, $http);
+});
+/* Services Add Controller */
+servicesapp.controller("ServicesAddController", function ($scope, $http, $location, $routeParams, sharedDataService) {
+    $scope.Header = "Добавление услуги";
+    $scope.addServices = function () {
+        var fd = new FormData();
+        fd.append('name', $scope.servicesusAdd.Name);
+        fd.append('desc', $scope.servicesusAdd.Desc);
+        fd.append('file', document.getElementsByName('servicesusPhoto')[0].files[0]);
+
+        $http.post('Services/AddServices', fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+    $scope.Back = function () { $location.path('/'); }
+});
+/* Services Edit Controller */
+servicesapp.controller("ServicesEditController", function ($scope, $http, $location, $routeParams, sharedDataService) {
+    $scope.Header = "Редактирование услуги";
+    $scope.itemForEdit = sharedDataService.getItem();
+    $scope.CurrentPhotoShow = true;
+
+    $scope.updateServices = function () {
+        var fd = new FormData();
+        fd.append('id', $scope.itemForEdit.idRecord);
+        fd.append('name', $scope.itemForEdit.name);
+        fd.append('desc', $scope.itemForEdit.description);
+        fd.append('oldphoto', $scope.itemForEdit.photoPath);
+        fd.append('file', document.getElementsByName('servicesPhoto')[0].files[0]);
+
+        $http.post('Services/UpdateServices', fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+    $scope.changePhoto = function () {
+        $scope.CurrentPhotoShow = false;
+        $scope.ChangePhotoShow = true;
+    };
+    $scope.Back = function () { $location.path('/'); };
+});
+/* Services Edit Photo Controller */
+servicesapp.controller("ServicesEditPhotoController", function ($scope, $http, $location, $routeParams, sharedDataService) {
+    $scope.Header = "Редактирование фотографий блока";
+    $scope.BlockPhotos = sharedDataService.getItem();
+    $scope.CurrentPhotoShow = true;
+
+    $scope.updatePhotoName = function (id) {
+        var name = document.getElementsByTagName('textarea')[id];
+
+    };
+
+    $scope.removePhoto = function (id) {
+        $http.post('Services/RemovePhotoFromBlock', { id: $scope.BlockPhotos.photos[id].idRecord }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+
+    $scope.addPhoto = function () {
+        var fd = new FormData();
+        fd.append('id', $scope.BlockPhotos.idRecord);
+        fd.append('name', $scope.servicesusPhotoAdd.Name);
+        fd.append('desc', $scope.servicesusPhotoAdd.Desc);
+        fd.append('file', document.getElementsByName('servicesusPhoto')[0].files[0]);
+
+        $http.post('Services/AddPhotoToBlock', fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).success(function (response) {
+            if (response === 'ok') {
+                $scope.servicesusPhotoAdd.Name = '';
+                $scope.servicesusPhotoAdd.Desc = '';
+                document.getElementsByName('servicesusPhoto').value = '';
+
+                $http.post('Services/GetBlockPhotos', { id: id }).success(function (response) {
+                    $scope.BlockPhotos = response;
+                });
+
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+
+    $scope.updateServices = function () {
+        var fd = new FormData();
+        fd.append('id', $scope.itemForEdit.idRecord);
+        fd.append('name', $scope.itemForEdit.name);
+        fd.append('desc', $scope.itemForEdit.description);
+        fd.append('oldphoto', $scope.itemForEdit.photoPath);
+        fd.append('file', document.getElementsByName('galleryPhoto')[0].files[0]);
+
+        $http.post('Services/UpdateServices', fd, {
+            transformRequest: angular.identity,
+            headers: { 'Content-Type': undefined }
+        }).success(function (response) {
+            if (response === 'ok') {
+                $location.path('/');
+            } else {
+                $scope.error = response;
+            }
+        });
+    };
+    $scope.changePhoto = function () {
+        $scope.CurrentPhotoShow = false;
+        $scope.ChangePhotoShow = true;
+    };
+    $scope.Back = function () { $location.path('/'); };
+});
+
+
+function GetAllServices($scope, $http) {
+    $('#loader').css({ "display": "block" });
+    $('.spinner').show();
+
+    $http.get('Services/GetAllServices').success(function (result) {
+        $scope.Services = result;
+    });
+
+    $('#loader').css({ "display": "none" });
+    $('.spinner').hide();
+}
