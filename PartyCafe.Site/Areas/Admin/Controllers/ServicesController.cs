366f64237237b1d2b13b4144b7d206cee6a745ad
Алексей <swtorigra@gmail.com>
2016-05-24 20:02:49 +0300

    Добавлен контроллер Услгу в админку, добавлены вьюхи. Изменен _layout админки

---
+++ PartyCafe.Site/Areas/Admin/Controllers/ServicesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyCafe.Site.Areas.Admin.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Admin/Services
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ServicesHome()
+        {
+            return View("ServicesHome");
+        }
+        [HttpGet]
+        public ActionResult ServicesAdd()
+        {
+            return View("ServicesAdd");
+        }
+        [HttpGet]
+        public ActionResult ServicesEdit()
+        {
+            return View("ServicesEdit");
+        }
+
+
+    }
+}
