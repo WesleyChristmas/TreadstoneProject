d489ea3161cc5e72f31a4ccc2f58a790305c9c63
Алексей <swtorigra@gmail.com>
2016-07-28 16:39:54 +0300

    Add user model, user utils controllers and view

---
+++ PartyCafe.Site/Areas/Admin/Core/Utils/UserUtils.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PartyCafe.Site.Identity;
+using PartyCafe.Site.Models;
+using PartyCafe.Site.Areas.Admin.Models;
+using Microsoft.AspNet.Identity;
+
+namespace PartyCafe.Site.Areas.Admin.Core.Utils
+{
+    public class UserUtils
+    {
+        private static RoleManager<Role> _roleManager = new RoleManager<Role>(new RoleStore());
+        private static UserManager<User> _userManager = new UserManager<User>(new UserStore());
+
+        public static List<User> GetAllUsers()
+        {
+            return _userManager.Users.ToList();
+        }
+        public static List<Role> GetAllRoles()
+        {
+            return _roleManager.Roles.ToList();
+        }
+
+        public static string ChangePassword(RegisterViewModel model, string oldPassword)
+        {
+            if (string.IsNullOrWhiteSpace(model.Password) || string.IsNullOrWhiteSpace(model.ConfirmPassword)) return "Поля не должны быть пустыми!";
+            if (model.Password != model.ConfirmPassword) return "Пароли не совпадают!";
+
+            var user = _userManager.FindByName(model.UserName);
+            var error = _userManager.ChangePassword(user.UserId.ToString(), oldPassword, model.Password);
+
+            return error.Succeeded ? "ok" : "bad";
+        }
+
+        public static UserDetail GetUserDetail(string username)
+        {
+            var result = new UserDetail();
+            result.user = new List<User>();
+
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                var user = _userManager.FindByName(username);
+                result.user.Add(new User()
+                {
+                    PasswordHash = null,
+                    SecurityStamp = null,
+                    UserId = Guid.Empty,
+                    UserName = user.UserName,
+                    Description = user.Description
+                });
+                var role = _userManager.GetRoles(user.UserId.ToString());
+                result.userroles = role.ToList();
+            }
+
+            return result;
+        }
+
+        public static string DeleteUser(string username)
+        {
+            var user = _userManager.FindByName(username);
+            _userManager.Delete(user);
+
+            return "ok";
+        }
+        public static string AddUser(RegisterViewModel model, string description, List<string> roles)
+        {
+            try
+            {
+                var user = new User()
+                {
+                    UserName = model.UserName,
+                    Description = description
+                };
+                var result = _userManager.Create(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    foreach (var item in roles)
+                    {
+                        _userManager.AddToRole(userId: user.UserId.ToString(), role: item);
+                    }
+                }
+                else
+                {
+                    return result.Errors.ToString();
+                }
+                return "ok";
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+       /* public static string EditUser(UserSaveEdit saveedit)
+        {
+            var OldSearchedUser = _userManager.FindByName(saveedit.oldname);
+            var userRoles = _userManager.GetRoles(userId: OldSearchedUser.UserId.ToString());
+
+            OldSearchedUser.UserName = saveedit.oldname != saveedit.newname ? saveedit.oldname : saveedit.newname;
+            OldSearchedUser.Description = saveedit.description;
+
+            var deleteRoles = userRoles.Except(saveedit.roles).ToList();
+            var createRoles = saveedit.roles.Except(userRoles).ToList();
+
+            foreach (var item in deleteRoles)
+            {
+                _userManager.RemoveFromRole(userId: OldSearchedUser.UserId.ToString(), role: item);
+            }
+
+            foreach (var item in createRoles)
+            {
+                _userManager.AddToRole(userId: OldSearchedUser.UserId.ToString(), role: item);
+            }
+
+            _userManager.Update(OldSearchedUser);
+
+            return "ok";
+        }*/
+    }
+}
---
+++ PartyCafe.Site/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using PartyCafe.Site.Models;
+
+namespace PartyCafe.Site.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public Guid RoleId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class UserDetail
+    {
+        public List<User> user { get; set; }
+        public List<Role> Roles { get; set; }
+        public List<string> userroles { get; set; }
+    }
+    public class UserSaveEdit
+    {
+        public string oldname { get; set; }
+        public string newname { get; set; }
+        public string rolename { get; set; }
+        public string description { get; set; }
+        public List<string> roles { get; set; }
+    }
+}
